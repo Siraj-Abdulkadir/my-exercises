ef5178ab39446c0f948fef80cdcf66d1fb014989
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2025-05-10 23:43:25 -0700

    Average of an array

---
+++ Java/Average_of_arrays.java
@@ -0,0 +1,17 @@
+public class Average_of_arrays{
+
+  public static void main (String[] args){
+
+        int[] numbersArrays={1,2,3,4,5,6,7,8};
+        int allsum=0;
+
+        for(int y=0; y<numbersArrays.length;y++){
+            int current= numbersArrays[y];
+            allsum = allsum + current;
+          }
+
+        int result=allsum/numbersArrays.length;
+        System.out.println(result);
+  }
+}
+
