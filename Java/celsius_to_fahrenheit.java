e6735bafa7037b3e507c2cadbc4442b479dbc0d1
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2025-05-10 23:39:11 -0700

    Celsius to Fahrenheit

---
+++ Java/celsius_to_fahrenheit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class celsius_to_fahrenheit {
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Enter the value of celsius: ");
+        double celsiusValue = userInput.nextDouble();
+
+        double fahrenheitValue = (celsiusValue*1.8) + 32;
+        System.out.println(celsiusValue + " Celsius in Fahrenheit is " + fahrenheitValue);
+    }
+     
+}
